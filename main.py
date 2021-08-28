6dd73d092210411efae9941ed1fefe7c29d84593
Sibints2000 <sibints2000@gmail.com>
2021-08-28 22:17:59 +0530

    Sixth Commit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import requests
 
-OWN_Endpoint = "https://api.openweathermap.org/data/2.5/onecall"
+OWM_Endpoint = "https://api.openweathermap.org/data/2.5/onecall"
 api_key = "9fcfa411c1742820ff1de86d72434f2c"
 
-requests.get()
+requests.get(OWM_Endpoint)
