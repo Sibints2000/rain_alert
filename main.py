605e800b8475b13897ea3f2f83b7ce3fb9c72fe2
Sibints2000 <sibints2000@gmail.com>
2021-08-28 22:38:59 +0530

    Thirteenth Commit

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ api_key = "9fcfa411c1742820ff1de86d72434f2c"
 weather_params = {
     "lat":  12.914142,
     "lon":  74.855957,
-    "appid":api_key
+    "appid": api_key
 }
 
 response = requests.get(OWM_Endpoint, params=weather_params)
