f840b3c3eb6c96328838c57c6e889ed30d627f66
Sibints2000 <sibints2000@gmail.com>
2021-08-28 22:52:05 +0530

    Twenty First Commit

--- main.py
+++ main.py
@@ -13,4 +13,5 @@ weather_params = {
 response = requests.get(OWM_Endpoint, params=weather_params)
 response.raise_for_status()
 weather_data = response.json()
-print(weather_data["hourly"][0]["weather"][0]["id"])
+weather_slice = weather_data["hourly"]
+# print(weather_data["hourly"][0]["weather"][0]["id"])
